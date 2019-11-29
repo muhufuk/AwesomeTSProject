b2c0c9554badf408e6c8160704d225bf3c80e9c6
muhufuk <veyselufukercin@gmail.com>
2019-11-29 19:53:34 +0300

    Create HomeScreen.tsx

---
+++ HomeScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { Props } from './App';
+
+export class HomeScreen extends React.Component<Props> {
+  static navigationOptions = {
+    title: 'Home',
+  };
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            this.props.navigation.navigate('Details', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            });
+          }}
+        />
+      </View>
+    );
+  }
+}
